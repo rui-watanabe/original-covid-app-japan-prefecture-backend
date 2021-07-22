47889b2bf4949623e5bd1043f62e506ce8f3b0d7
rui-watanabe <wataru610806@gmail.com>
2021-07-22 14:45:59 +0900

    add TestInitExportApiData #4

--- api/data/api_test.go
+++ api/data/api_test.go
@@ -44,3 +44,36 @@ func TestGetExportApiData(t *testing.T) {
 		t.Errorf("%v != %v", count, ansCount)
 	}
 }
+
+func TestInitExportApiData(t *testing.T) {
+	data := initExportApiData()
+	n1 := data.Hokkaido.OutPatient.Normal
+	n2 := 1
+	if n1 != n2 {
+		t.Errorf("%v != %v", n1, n2)
+	}
+
+	l1 := data.Hokkaido.OutPatient.Limit
+	l2 := 2
+	if l1 != l2 {
+		t.Errorf("%v != %v", l1, l2)
+	}
+
+	s1 := data.Hokkaido.OutPatient.Stopped
+	s2 := 3
+	if s1 != s2 {
+		t.Errorf("%v != %v", s1, s2)
+	}
+
+	ui1 := data.Hokkaido.OutPatient.Unintroduced
+	ui2 := 4
+	if ui1 != ui2 {
+		t.Errorf("%v != %v", ui1, ui2)
+	}
+
+	ua1 := data.Hokkaido.OutPatient.Unanswered
+	ua2 := 5
+	if ua1 != ua2 {
+		t.Errorf("%v != %v", ua1, ua2)
+	}
+}
